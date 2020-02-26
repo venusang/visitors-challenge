35df17ba835ad9acc62f3712395daedabdc2b8f1
Venus Ang <venusang24@gmail.com>
2020-02-25 18:12:40 -0800

    DRYing up acceptance test - moved visit to method

--- tests/acceptance/visitors-challenge-test.js
+++ tests/acceptance/visitors-challenge-test.js
@@ -10,8 +10,9 @@ import { setupMirage } from "ember-cli-mirage/test-support";
 
 import { module, test } from "qunit";
 
-function setUpServer(server) {
+async function setUpServer(server) {
   server.createList("entry", 3);
+  await visit("/");
 }
 
 module("Acceptance | visitors-challenge", function(hooks) {
@@ -19,14 +20,12 @@ module("Acceptance | visitors-challenge", function(hooks) {
   setupMirage(hooks);
 
   test("Listing visitors: I can view all visitors who have been signed-in", async function(assert) {
-    setUpServer(this.server);
-    await visit("/");
+    await setUpServer(this.server);
     assert.dom("[data-test-id='entry-row']").exists({ count: 3 });
   });
 
   test("Add a new visitor: As a Cool Chip Employee that is viewing the visitors list, when I click `Add a New Visitor` and I provide the first name, last name and notes about the visitor, and I choose to save, Then I see the new visitor added to the list of all visitors that is not signed out.", async function(assert) {
-    setUpServer(this.server);
-    await visit("/");
+    await setUpServer(this.server);
     await fillIn("input#firstname", "Joe");
     await fillIn("input#lastname", "Coffee");
     await fillIn("input#notes", "Joe likes coffee");
@@ -49,16 +48,14 @@ module("Acceptance | visitors-challenge", function(hooks) {
   });
 
   test("Signing out visitors: As a Cool Chip employee that is viewing the visitors list, when I click the Sign Out button for a specific visitor, then the visitor is marked as signed out", async function(assert) {
-    setUpServer(this.server);
-    await visit("/");
+    await setUpServer(this.server);
     await click(".sign-out-btn");
     let signOutText = findAll("[data-test-id='visitor-sign-out']")[0];
     assert.dom(".visitor-sign-out").hasText(signOutText.innerHTML);
   });
 
   test("Search: The user can search the current visitors list and the list will only show visitors who are not signed out", async function(assert) {
-    setUpServer(this.server);
-    await visit("/");
+    await setUpServer(this.server);
     await click(".sign-out-btn");
 
     await fillIn("input.search-text", "Signed out");
