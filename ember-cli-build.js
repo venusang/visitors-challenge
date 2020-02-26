40e588ae4caa97e280318b64d4c6dcbab92fd975
Venus Ang <venusang24@gmail.com>
2020-02-25 18:55:38 -0800

    disabling fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,16 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
-module.exports = function (defaults) {
+module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     sassOptions: {
       extension: "scss"
+    },
+    fingerprint: {
+      enabled: false
     }
   });
-
 
   return app.toTree();
 };
